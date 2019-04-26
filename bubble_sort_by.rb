16ce984deeec0aa31b8aafc2a9a642191c311e01
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-04-27 00:23:36 +0100

    Build the method for bubble_sort_by
    the yield keyword was used to allow the method to accept block

---
+++ bubble_sort_by.rb
@@ -0,0 +1,20 @@
+def bubble_sort_by(array)
+  not_sorted = true
+  n = array.length - 1
+  while not_sorted
+    not_sorted = false
+    (0...n).each do |i|
+      next unless yield(array[i], array[i + 1]) > 0
+
+      array[i], array[i + 1] = array[i + 1], array[i]
+      not_sorted = true
+    end
+    n -= 1
+  end
+  p array
+end
+
+bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+  left.length <=> right.length
+end
+# ["hi", "hey", "hello"]
